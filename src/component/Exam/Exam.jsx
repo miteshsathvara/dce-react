4b69d1e35264790474cb3153e7607e76a155f3f4
“Mitesh <mitesh0267@gmail.com>
2024-07-21 22:53:04 +0530

    issue fixed and commented code

--- src/component/Exam/Exam.jsx
+++ src/component/Exam/Exam.jsx
@@ -7,29 +7,64 @@ import ConfirmDialog from './ConfirmDialog';
 
 
 const Exam = () => {
-  const navigate = useNavigate();
 
+  // Start Header code
+  // Use for navigation
+  const navigate = useNavigate();
+  // Logout Function
   const logout = (e) => {
-    // prevent the form from refreshing the whole page
     e.preventDefault();
+    // Clear all localstorage
     Object.keys(localStorage).forEach(key => {
       if (key.startsWith('selectedAnswer-')) {
         
         localStorage.removeItem(key);
-      } 
+      }
     });
     localStorage.removeItem('token');
     localStorage.removeItem('type_id');
     toast.success("Logout Successfully.", {
       autoClose: 1500,
     });
+    // redirect to home page
     navigate('/');
-  }
+  };
+   // Get Username on blue header part
+   const [apiData, setApiData] = useState([]);
+   useEffect(() => {
+     const fetchProfile = async () => {
+       try {
+         const response = await api.get('/getUserExamDetail');
+         setApiData(response.data.data);
+       } catch (error) {
+         // Handle error or redirect to login
+       }
+     };
+ 
+     fetchProfile();
+   }, []);
+   // get current date
+   const getCurrentDate = () => {
+     const today = new Date();
+     const dd = String(today.getDate()).padStart(2, '0');
+     const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+     const yyyy = today.getFullYear();
+ 
+     return `${dd}/${mm}/${yyyy}`;
+   };
+   const currentDate = getCurrentDate();
+ 
+  // End Header Code
+  // Start Question Logic
+  // get Type id from localstorage
   const type_id = localStorage.getItem('type_id');
-
+  // questionData used for all question get from apis
   const [questionData, setquestionData] = useState([]);
+  // currentQuestionIndex used for next and back button
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  // once user select answer thenset in this state and pass to API
   const [answers, setAnswers] = useState({});
+  // Once final question submit display dialog box
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
 
   useEffect(() => {
@@ -37,14 +72,18 @@ const Exam = () => {
       try {
         const response = await api.get('/get_questions/' + type_id);
         const initialAnswers = {};
-        response.data.data.forEach((question) => {
+        response.data.data.forEach((question, index) => {
+          // all question selectedAnswer set in localstorage
           const savedAnswer = localStorage.getItem(`selectedAnswer-${question.order_no}`);
           const answer = savedAnswer ? savedAnswer : question.attempted_answer;
           initialAnswers[question.order_no] = answer;
           localStorage.setItem(`selectedAnswer-${question.order_no}`, answer);
+          // for shuffled question set index for each question
+          question.index = index + 1; 
         });
-        
+        // Api Response set in answers state
         setAnswers(initialAnswers);
+        // Api Response set in questionData state
         setquestionData(response.data.data);
       } catch (error) {
         // Handle error or redirect to login
@@ -53,60 +92,22 @@ const Exam = () => {
 
     fetchQuestions();
   }, []);
-
-
-  const [apiData, setApiData] = useState([]);
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const response = await api.get('/getUserExamDetail');
-        // const initialAnswers = {};
-        // response.data.data.forEach((question) => {
-        //   const savedAnswer = localStorage.getItem(`selectedAnswer-${question.order_no}`);
-        //   const answer = savedAnswer ? savedAnswer : question.attempted_answer;
-        //   initialAnswers[question.order_no] = answer;
-        //   localStorage.setItem(`selectedAnswer-${question.order_no}`, answer);
-        // });
-        setApiData(response.data.data);
-      } catch (error) {
-        // Handle error or redirect to login
-      }
-    };
-
-    fetchProfile();
-  }, []);
-
-  const getCurrentDate = () => {
-    const today = new Date();
-    const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
-    const yyyy = today.getFullYear();
-
-    return `${dd}/${mm}/${yyyy}`;
-  };
-  const currentDate = getCurrentDate();
-
-  const startExam = (e) => {
-    // prevent the form from refreshing the whole page
-    e.preventDefault();
-    navigate('/');
-  }
-  
+  // Handle Next Question Logic 
   const handleNextQuestion = async (e) => {
-
     e.preventDefault();
+    // get answer from localstorage
     const answer = localStorage.getItem(`selectedAnswer-${currentQuestion?.order_no}`);
 
-    if (currentQuestion.order_no == questionData[questionData.length - 1]?.order_no) {
+    if (currentQuestion.order_no === questionData[questionData.length - 1]?.order_no) {
+      // Last Question display show dialog box
       setShowConfirmDialog(true);
     } else {
+      // attempt logic
       let question_id = currentQuestion?.id;
-      // Attempt Api call
       await api.post('/attemptquiz/' + type_id, {
         answer,
         question_id
       }).then((result) => {
-        
         if (result.data.status === "Success") {
           toast.success(result.data.data, {
             autoClose: 2000,
@@ -114,7 +115,6 @@ const Exam = () => {
           setCurrentQuestionIndex(currentQuestionIndex + 1);
         }
       }).catch((error) => {
-
         if (error.response.data.status === "Failed") {
           toast.error(error.response.data.message, {
             autoClose: 2000,
@@ -123,42 +123,40 @@ const Exam = () => {
       });
     }
   };
+  // on dialog confirm action
   const handleConfirm = () => {
     setShowConfirmDialog(false);
     navigate('/thankyou');
   };
-
+  // on dialog cancel action
   const handleCancel = () => {
     toast.info('You are still on the current question.');
     setShowConfirmDialog(false);
   };
+  // On change radio button set answer in localstorage
   const handleOptionChange = (order_no, option) => {
     const newAnswers = { ...answers, [order_no]: option };
     setAnswers(newAnswers);
     localStorage.setItem(`selectedAnswer-${order_no}`, option);
   };
+  // On click on back question logic
   const handleBackQuestion = (e) => {
     e.preventDefault();
-    
     if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
     } else {
       alert('No Any Questions.');
     }
   };
-
+  // Click on particular question redirection
   const handleRedirectQuestion = (q) => {
-    setCurrentQuestionIndex(q.order_no - 1);
+    setCurrentQuestionIndex(q.index - 1);
   }
-
+  // set current question from all question data
   const currentQuestion = questionData[currentQuestionIndex];
 
-  // if (questions.length === 0) {
-  //   return <div>Loading...</div>;
-  // }
   return (
     <div>
-      {/*  Start Main Contents */}
       <main>
         <div className="blue">
           <div className="human-pic">
@@ -174,7 +172,7 @@ const Exam = () => {
       <div className="container">
         <div className="question-container">
           <form action="#">
-            <p><span>{currentQuestion?.order_no} .</span>{currentQuestion?.question}</p>
+            <p><span>{currentQuestion?.index}.</span>{currentQuestion?.question}</p>
             {currentQuestion?.answer?.map(option => (
               <div key={option.options}>
                 <label>
@@ -183,10 +181,7 @@ const Exam = () => {
                     name={`question-${currentQuestion.order_no}`}
                     value={option?.options}
                     checked={answers[currentQuestion.order_no] === option.options}
-                    // checked={currentQuestion.attempted_answer == option?.options}
                     onChange={() => handleOptionChange(currentQuestion.order_no, option.options)}
-                  // onChange={handleOptionChange}
-
                   />
                   {option?.options}
                 </label>
@@ -194,10 +189,9 @@ const Exam = () => {
             ))}
             <div className="question-button">
               <button onClick={(e) => handleBackQuestion(e)} >Back</button>
-              <button onClick={(e) => handleNextQuestion(e)} style={{ 'background-color': "rgb(0, 132, 255)" }}>
+              <button onClick={(e) => handleNextQuestion(e)} style={{ backgroundColor: "rgb(0, 132, 255)" }}>
                 SUBMIT &amp; NEXT
               </button>
-
             </div>
             <ConfirmDialog
               isVisible={showConfirmDialog}
@@ -210,16 +204,16 @@ const Exam = () => {
           <div className="question-layout">
             <div className="title">Question-Layout</div>
             <div className="questions">
-              {questionData.map(q => {
+              {questionData.map((q, index) => {
                 let style = {};
-                if (q.order_no === currentQuestion?.order_no) {
+                if (index === currentQuestion?.index - 1) {
                   style = { backgroundColor: "orangered", color: "white" };
                 } else if (q.attempted) {
                   style = { backgroundColor: 'rgb(0, 132, 255)', color: 'white' };
                 }
                 return (
                   <div key={q.id} style={style} onClick={() => handleRedirectQuestion(q)}>
-                    {q.order_no}
+                    {index + 1}
                   </div>
                 );
               })}
@@ -227,8 +221,8 @@ const Exam = () => {
           </div>
           <div className="summery">
             <div className="indication">
-              <div style={{ 'background-color': "orangered", color: "white" }}></div><p>Current Question</p>
-              <div style={{ 'background-color': "rgb(0, 132, 255)", color: "white" }} />
+              <div style={{ backgroundColor: "orangered", color: "white" }}></div><p>Current Question</p>
+              <div style={{ backgroundColor: "rgb(0, 132, 255)", color: "white" }} />
               <p>Attempted</p>
               <div />
               <p>Not Attempted</p>
@@ -236,7 +230,6 @@ const Exam = () => {
           </div>
         </div>
       </div>
-      {/* End Main Contents */}
     </div>
   );
 };
